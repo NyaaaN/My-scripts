1feda81b7677c5cfa3605ee31eba182d968dedc1
Nyan ^__^ <wardream12@gmail.com>
2014-09-29 00:30:53 +0400

    Create NyanRuneMaker.lua

---
+++ NyanRuneMaker.lua
@@ -0,0 +1,40 @@
+local nLast = 0	
+local runes = {}
+runes[-2272] = drawMgr:CreateRect(104,901,16,16,-1)	runes[-2272].visible = false 
+runes[3008] = drawMgr:CreateRect(198,978,16,16,-1) runes[3008].visible = false 
+  
+function Main()
+    if not client.connected or client.loading or client.console then return end
+	local nRunes = entityList:FindEntities({classId=CDOTA_Item_Rune})
+	if #nRunes ~= nLast then 
+		runes[-2272].visible = false 
+		runes[3008].visible = false 
+	else
+		return
+	end
+
+	for i,rune in ipairs(nRunes) do
+		local runename = ""
+			
+		if rune.runeType == 0 then runename = "doubledamage" 
+		elseif rune.runeType == 1 then runename = "haste" 
+		elseif rune.runeType == 2 then runename = "illusion" 
+		elseif rune.runeType == 3 then runename = "invis"
+		elseif rune.runeType == 4 then runename = "regen"
+		elseif rune.runeType == 5 then runename = "bounty"
+		end
+		
+		runes[rune.position.x].textureId = drawMgr:GetTextureId("NyanUI/minirunes/"..runename) 
+		runes[rune.position.x].visible = true	
+	end		
+	nLast = #nRunes
+end
+
+function GameClose()
+	nLast = nil
+	runes = nil
+	collectgarbage("collect")
+end
+ 
+script:RegisterEvent(EVENT_CLOSE,GameClose)
+script:RegisterEvent(EVENT_TICK,Main)
